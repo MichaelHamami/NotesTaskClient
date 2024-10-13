4ffb6a9b942dfa2e4fd1095c54e581923ac1879d
Michael Hamami <hamami2010@gmail.com>
2024-10-14 01:37:40 +0300

    feat: render lines in editor

--- src/components/NotesProjectComponent/Note/NoteEditor.js
+++ src/components/NotesProjectComponent/Note/NoteEditor.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet, Dimensions } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import NoteHeader from './NoteHeader';
 import { getNoteById } from 'redux/selectors/note.selectors';
@@ -10,6 +10,8 @@ import * as NoteActions from 'redux/actions/note.actions';
 import { showToast } from 'utils/helpers';
 import { removeComponentsDetailsFromNoteContent } from './noteHelper';
 
+const { width } = Dimensions.get('window');
+
 const NoteEditor = ({ route }) => {
   const currentNote = useSelector(state => getNoteById(state, route.params.noteId));
   const dispatch = useDispatch();
@@ -18,6 +20,7 @@ const NoteEditor = ({ route }) => {
   const [showCreateTask, setShowCreateTask] = useState(false);
   const [cursorPosition, setCursorPosition] = useState(0);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [inputHeight, setInputHeight] = useState(0);
 
   const handleNoteChange = text => {
     if (!errorMessage) {
@@ -26,7 +29,6 @@ const NoteEditor = ({ route }) => {
     setNoteContent(text);
   };
 
-  // Capture cursor position when selection changes
   const handleSelectionChange = event => {
     const { start } = event.nativeEvent.selection;
     setCursorPosition(start);
@@ -42,8 +44,6 @@ const NoteEditor = ({ route }) => {
   };
 
   const validateTask = () => {
-    // Validate task data
-
     return true;
   };
 
@@ -61,7 +61,6 @@ const NoteEditor = ({ route }) => {
   };
 
   const handleCreateTask = async task => {
-    // Validate task data
     if (!validateTask(task)) {
       setShowCreateTask(false);
       setErrorMessage('Task data is invalid.');
@@ -77,7 +76,6 @@ const NoteEditor = ({ route }) => {
         });
       }
     } catch (error) {
-      console.error('Error creating task:', error);
       setErrorMessage('Error creating task. Please try again later.');
     } finally {
       setIsLoading(false);
@@ -85,19 +83,31 @@ const NoteEditor = ({ route }) => {
     }
   };
 
+  const renderLines = () => {
+    const lines = [];
+    const lineSpacing = 15;
+    const numberOfLines = Math.floor(inputHeight / lineSpacing);
+    for (let i = 0; i < numberOfLines; i++) {
+      lines.push(<View key={i} style={[styles.line, { top: i * lineSpacing }]} />);
+    }
+    return lines;
+  };
+
+  const onLayout = event => {
+    const { height } = event.nativeEvent.layout;
+    setInputHeight(height);
+  };
+
   if (!currentNote) return <Text> ...No Content</Text>;
   return (
     <View>
       <NoteHeader note={currentNote} isOnEditMode={true} />
-      <TextInput
-        // style={styles.textInput}
-        multiline
-        value={noteContent}
-        onChangeText={handleNoteChange}
-        onSelectionChange={handleSelectionChange}
-      />
+      <View style={styles.linesAndTextInput} onLayout={onLayout}>
+        <View style={styles.linesContainer}>{renderLines()}</View>
+        <TextInput style={styles.textInput} multiline value={noteContent} onChangeText={handleNoteChange} onSelectionChange={handleSelectionChange} />
+      </View>
       <View style={{ gap: 10 }}>
-        <Button title="Add Task" onPress={openTaskModal} uppercase={false} capitalize={false} />
+        <Button title="Add Task" onPress={openTaskModal} />
         <Button title="Save Note" onPress={handleSaveNote} />
         {errorMessage && <Text>{errorMessage}</Text>}
       </View>
@@ -105,5 +115,27 @@ const NoteEditor = ({ route }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  linesAndTextInput: {},
+  linesContainer: {
+    position: 'absolute',
+    top: 28,
+    left: 10,
+    right: 10,
+    bottom: 20,
+  },
+  line: {
+    height: 1.5,
+    width: width,
+    backgroundColor: '#d3d3d3',
+  },
+  textInput: {
+    fontSize: 14,
+    color: 'black',
+    backgroundColor: 'transparent',
+    textAlignVertical: 'top',
+  },
+});
 
 export default NoteEditor;
