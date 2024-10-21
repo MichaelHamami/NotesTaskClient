c1416967b96462c2c6eac2fd632191df5cdb549d
Michael Hamami <hamami2010@gmail.com>
2024-10-22 01:50:40 +0300

    feat: show task in editor

--- src/components/NotesProjectComponent/Note/NoteEditor.js
+++ src/components/NotesProjectComponent/Note/NoteEditor.js
@@ -7,8 +7,17 @@ import TaskModal from '../Task/TaskModal';
 import { createTask } from 'api/task.api';
 import { updateNote } from 'api/note.api';
 import * as NoteActions from 'redux/actions/note.actions';
-import { showToast } from 'utils/helpers';
+import { showToast, splitStringAtFirstOccurrence } from 'utils/helpers';
 import { removeComponentsDetailsFromNoteContent } from './noteHelper';
+import TaskNote from '../Task/TaskNote';
+
+const Task = ({ data }) => {
+  const [_componentName, restOfContent] = splitStringAtFirstOccurrence(data, ':');
+  const [noteId, noteData] = splitStringAtFirstOccurrence(restOfContent, ':');
+  const jsonNote = noteData.substring(0, noteData.length - 1);
+
+  return <TaskNote {...JSON.parse(jsonNote)} componentId={noteId} />;
+};
 
 const { width } = Dimensions.get('window');
 
@@ -16,22 +25,17 @@ const NoteEditor = ({ route }) => {
   const currentNote = useSelector(state => getNoteById(state, route.params.noteId));
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
-  const [noteContent, setNoteContent] = useState(removeComponentsDetailsFromNoteContent(currentNote?.content));
+  const [noteContent, setNoteContent] = useState(currentNote?.content);
   const [showCreateTask, setShowCreateTask] = useState(false);
   const [cursorPosition, setCursorPosition] = useState(0);
   const [errorMessage, setErrorMessage] = useState(null);
   const [inputHeight, setInputHeight] = useState(0);
+  const [lastSegmentChanged, setLastSegmentChanged] = useState(0);
 
-  const handleNoteChange = text => {
-    if (!errorMessage) {
-      setErrorMessage(null);
-    }
-    setNoteContent(text);
-  };
-
-  const handleSelectionChange = event => {
+  const handleSelectionChange = (index, event) => {
     const { start } = event.nativeEvent.selection;
     setCursorPosition(start);
+    setLastSegmentChanged(index);
   };
 
   const openTaskModal = () => {
@@ -51,7 +55,8 @@ const NoteEditor = ({ route }) => {
     setIsLoading(true);
 
     try {
-      const noteData = await updateNote(currentNote._id, { content: noteContent });
+      const formattedNoteContent = removeComponentsDetailsFromNoteContent(noteContent);
+      const noteData = await updateNote(currentNote._id, { content: formattedNoteContent });
       dispatch(NoteActions.updateNote(currentNote._id, noteData));
       showToast('Note saved successfully', true);
     } catch (error) {
@@ -71,9 +76,20 @@ const NoteEditor = ({ route }) => {
       const taskPayload = { ...task, note: currentNote._id };
       const taskData = await createTask(taskPayload);
       if (taskData) {
-        setNoteContent(prev => {
-          return prev.substring(0, cursorPosition) + `<Task:${taskData._id}>` + prev.substring(cursorPosition);
-        });
+        const segements = getNoteDataAsSegments();
+        let updatedNoteContent = '';
+        for (let i = 0; i < segements.length; i++) {
+          if (i === lastSegmentChanged) {
+            const newSegmentText =
+              segements[i].content.substring(0, cursorPosition) +
+              `<Task:${taskData._id}:${JSON.stringify(taskData)}>` +
+              segements[i].content.substring(cursorPosition);
+            updatedNoteContent += newSegmentText;
+            continue;
+          }
+          updatedNoteContent += segements[i].content;
+        }
+        setNoteContent(updatedNoteContent);
       }
     } catch (error) {
       setErrorMessage('Error creating task. Please try again later.');
@@ -83,37 +99,72 @@ const NoteEditor = ({ route }) => {
     }
   };
 
-  const renderLines = () => {
-    const lines = [];
-    const lineSpacing = 15.4;
-    const numberOfLines = Math.floor(inputHeight / lineSpacing);
-    for (let i = 0; i < numberOfLines; i++) {
-      lines.push(<View key={i} style={[styles.line, { top: i * lineSpacing }]} />);
+  const getNoteDataAsSegments = () => {
+    const regex = /(<[^>]+>)/g; // Regex to find Components
+    const segments = [];
+    let lastIndex = 0;
+    let match;
+
+    while ((match = regex.exec(noteContent)) !== null) {
+      if (lastIndex !== match.index) {
+        segments.push({
+          type: 'text',
+          content: noteContent.slice(lastIndex, match.index),
+        });
+      }
+
+      segments.push({
+        type: 'task',
+        content: match[1],
+      });
+      lastIndex = regex.lastIndex;
     }
-    return lines;
+
+    if (lastIndex < noteContent.length) {
+      segments.push({
+        type: 'text',
+        content: noteContent.slice(lastIndex),
+      });
+    }
+    return segments;
   };
 
-  const onLayout = event => {
-    const { height } = event.nativeEvent.layout;
-    setInputHeight(height);
+  const handleTextChange = (index, newText) => {
+    if (!errorMessage) {
+      setErrorMessage(null);
+    }
+
+    const segments = getNoteDataAsSegments();
+    segments[index].content = newText;
+    const updatedNoteData = segments.map(segment => segment.content).join('');
+    setNoteContent(updatedNoteData);
+  };
+
+  const renderNote = () => {
+    const segments = getNoteDataAsSegments();
+    return segments.map((segment, index) => {
+      if (segment.type === 'text') {
+        return (
+          <TextInput
+            key={index}
+            style={styles.textInput}
+            multiline
+            value={segment.content}
+            onChangeText={text => handleTextChange(index, text)}
+            onSelectionChange={event => handleSelectionChange(index, event)}
+          />
+        );
+      } else if (segment.type === 'task') {
+        return <Task key={index} data={segment.content} />;
+      }
+    });
   };
 
   if (!currentNote) return <Text> ...No Content</Text>;
   return (
     <View style={styles.container}>
       <NoteHeader note={currentNote} isOnEditMode={true} />
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.linesAndTextInput} onLayout={onLayout}>
-          <View style={styles.linesContainer}>{renderLines()}</View>
-          <TextInput
-            style={styles.textInput}
-            multiline
-            value={noteContent}
-            onChangeText={handleNoteChange}
-            onSelectionChange={handleSelectionChange}
-          />
-        </View>
-      </ScrollView>
+      <ScrollView contentContainerStyle={styles.scrollView}>{renderNote()}</ScrollView>
       <View style={styles.footer}>
         <Button title="Add Task" onPress={openTaskModal} />
         <Button title="Save Note" onPress={handleSaveNote} />
